3337e9909dbf6ddb1296a3d2710080a1f5fcef70
sean-garwood <seansgarwood@gmail.com>
2024-10-22 15:51:36 -0400

    chore: new/create kitten
    * form
    * show new kitten

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -8,14 +8,27 @@ class KittensController < ApplicationController
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def edit
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to kitten_path(@kitten)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
+  end
+
+  private
+
+  def kitten_params
+    params.fetch(:kitten, {}).permit(:name, :age, :cuteness, :softness)
   end
 end
