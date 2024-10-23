155d5098b4342a5483b77d6f4b14fb8d5e0cd076
sean-garwood <seansgarwood@gmail.com>
2024-10-23 13:25:48 -0400

    feat: too perfect validation on kittens
    * fun easter egg
    
    * separate fields with divs

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,4 +1,11 @@
 class Kitten < ApplicationRecord
   validates :name, :age, :cuteness, :softness, presence: true
   validates :age, numericality: { greater_than: 0 }
+  validates :cuteness, :softness, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+
+  validate do |kitten|
+    if kitten.cuteness == 10 && kitten.softness == 10
+      kitten.errors.add(:base, "This kitten is too perfect to exist.")
+    end
+  end
 end
