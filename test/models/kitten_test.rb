fc900371a60845f4e3fd149cf6ba6941b6e2117e
sean-garwood <seansgarwood@gmail.com>
2024-10-23 14:24:25 -0400

    fix: rewrite tests
    * for too-cute kittens

--- test/models/kitten_test.rb
+++ test/models/kitten_test.rb
@@ -1,15 +1,27 @@
 require "test_helper"
 
 class KittenTest < ActiveSupport::TestCase
-  test "should save kitten" do
+  test "should not save perfect kitten" do
     kitten = Kitten.new(name: "test", age: 1, cuteness: 10, softness: 10)
     kitten.save
-    assert kitten.valid?
+    assert_not kitten.valid?
   end
 
   test "kitten cannot have negative age" do
     kitten = Kitten.new(name: "test", age: -1, cuteness: 10, softness: 10)
     kitten.save
     assert_not kitten.valid?
+  end
+
+  test "kitten cannot have cuteness or softness less than 1" do
+    kitten = Kitten.new(name: "test", age: 1, cuteness: 0, softness: 0)
+    kitten.save
+    assert_not kitten.valid?
+  end
+
+  test "valid kitten is valid" do
+    kitten = Kitten.new(name: "test", age: 1, cuteness: 1, softness: 1)
+    kitten.save
+    assert kitten.valid?
   end
 end
